4ba4d42c0d6b96831f515f4dcb6907f38e3b1b6c
bigminerplay <32434395+bigminerplay@users.noreply.github.com>
2021-07-11 23:10:19 +0200

    add Logger.java add startup massage

--- src/main/java/de/cericx/coreapi/CoreApi.java
+++ src/main/java/de/cericx/coreapi/CoreApi.java
@@ -1,12 +1,15 @@
 package de.cericx.coreapi;
 
+import de.cericx.coreapi.logger.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CoreApi extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        
+
+        new Logger("CoreApi by Cericx_ and RudiBott started\nVersion: 1.0\nSince 11.07.2021\nThank you for Using").info();
+
         super.onEnable();
     }
 }
